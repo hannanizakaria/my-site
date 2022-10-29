76836d8d69da7750bdd2892b4ed5c369de652ac0
hannanizakaria <109367862+hannanizakaria@users.noreply.github.com>
2022-10-29 18:04:20 +0000

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*=============== Active Link =============== */
+const linkWork = document.querySelectorAll('.category__btn');
+
+function activeWork() {
+    linkWork.forEach((a) => a.classList.remove('active-work'));
+    this.classList.add('active-work');
+}
+
+linkWork.forEach((a) => a.addEventListener('click', activeWork))
+
+
+
+
+
+/*=============== Mixitup Filter =============== */
+let mixerProjects = mixitup('.projects__container', {
+    selectors: {
+        target: '.projects__item'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+
+
+
+
+
+/*=============== Testimonials Swiper =============== */
